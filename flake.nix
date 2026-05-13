bf31525d338ae71871d89e69bf51b21b3be38a76
Rasmus <rasmus@egenintres.se>
2026-05-13 14:09:15 +0200

    chore: update kube.just and flake.nix for improved Kubernetes management and add nix-flux-local support

--- flake.nix
+++ flake.nix
@@ -4,13 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
+    nix-flux-local.url = "github:Jaeensson/nix-flux-local";
   };
 
   outputs =
     inputs@{
       nixpkgs,
       flake-utils,
+      nix-flux-local,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -19,11 +20,17 @@
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
+          overlays = [
+            (final: prev: {
+              flux-local = nix-flux-local.packages.${system}.flux-local;
+            })
+          ];
         };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            flux-local
             terraform
             tflint
             kubectl
